36742af3794816dddc8d38764538104e4ae1dbd8
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-06-11 08:32:38 +0700

    Create Dark.sh

---
+++ Dark.sh
@@ -0,0 +1,20 @@
+
+clear
+figlet "DARK"
+echo "###############################################"|lolcat
+echo "AUTHOR : FAJAR CYBER"|lolcat
+echo "KONTAK : 08817805411"|lolcat
+echo "GITHUB : FAJAR-24"|lolcat
+echo "##############################################"|lolcat
+echo "SEBELUM LOGIN ANDA HARUS LOGIN DI BROWSER DULU"
+echo "AGAR AKUN ANDA TIDAK KENA CHECKPOINT"
+echo
+read -p " USERNAME : " read tr
+read -p " PASSWORD : " read tr
+echo
+echo "SEDANG MASUK...."|lolcat
+echo "GAGAL"|lolcat
+echo
+echo "AKUN ANDA KENA CHECKPOINT"
+echo "SILAKAN LOGIN FB KAMU DULU DI BROWSER / OPERAMINI"|lolcat
+echo "AGAR TIDAK KENA CHECKPOINT"|lolcat
